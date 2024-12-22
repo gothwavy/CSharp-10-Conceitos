2e598426beffc302b2d03243759a995dd0312cea
gothwavy <ryanalmeidadev@gmail.com>
2024-12-22 18:05:17 -0300

    Finalizado✅

---
+++ Calculadora.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _10_AtributosMetodosEstaticos
+{
+    internal class Calculadora
+    {
+        // Atributo numerico
+        public static int numero = 10;
+
+        // Metodo para somar dois valores
+        public static void Soma(int n1, int n2)
+        {
+            Console.WriteLine($"A soma dos valores e: {n1 + n2}");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿namespace _10_AtributosMetodosEstaticos
+{
+    class Program()
+    {
+        static void Main(string[] args)
+        {
+            // Exibir o valor contido no atributo numero
+            Console.WriteLine($"O valor e: {Calculadora.numero}");
+
+            // Utilizando o metodo soma
+            Calculadora.Soma(10, 8);
+
+            // O metodo static faz com que um atributo ou um metodo de uma classe fique disponivel para todas as outras classes sem que voce precise instanciar um objeto.
+        }
+    }
+}
